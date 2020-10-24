c1276efd96457a082464dd363fea8a3b280cdaf1
krsnucc21 <73293364+krsnucc21@users.noreply.github.com>
2020-10-24 20:55:58 +0900

    Update wordfreq.js

--- wordfreq.js
+++ wordfreq.js
@@ -1,6 +1,8 @@
+const ID_WORD_FREQUENCY_COUNTERS = "word_freq_counters";
+const LIST_WORD_FREQUENCY_COUNTERS = document.getElementById(ID_WORD_FREQUENCY_COUNTERS);
 const ID_WORD_FREQUENCY_LIST = "word_freq_list";
+const LIST_WORD_FREQUENCY_LIST = document.getElementById(ID_WORD_FREQUENCY_LIST);
 const ID_INPUT_TEXT = "input_text";
-const LIST_WORD_FREQUENCY = document.getElementById(ID_WORD_FREQUENCY_LIST);
 
 function tokenize(text) {
     // Modified regex from Mike Grace's answer on StackOverflow:
@@ -9,8 +11,7 @@ function tokenize(text) {
     return tokenizedText;
 }
 
-function wordFreq(text) {
-    const tokenizedText = tokenize(text);
+function wordFreq(tokenizedText) {
     let freq = {};
     for (let i = 0; i < tokenizedText.length; i++) {
         if (freq[tokenizedText[i]]) {
@@ -23,19 +24,36 @@ function wordFreq(text) {
 }
 
 function showWordFreq() {
-    LIST_WORD_FREQUENCY.innerHTML = "";
     const DATA = document.getElementById(ID_INPUT_TEXT).value;
-    const OBJECT_WORD_FREQUENCY = wordFreq(DATA);
-    let keys = [];
-    let values = [];
-    //OBJECT_WORD_FREQUENCY.Sort();
-    for (let [key, value] of Object.entries(OBJECT_WORD_FREQUENCY)) {
-        keys.push(key);
-        values.push(value);
+    const TOKENIZED_DATA = tokenize(DATA);
+    const OBJECT_WORD_FREQUENCY = wordFreq(TOKENIZED_DATA);
+
+    let wordArray = [];
+    wordArray = Object.keys(OBJECT_WORD_FREQUENCY).map(function (key) {
+        return {
+            name: key,
+            total: OBJECT_WORD_FREQUENCY[key]
+        };
+    });
+
+    wordArray.sort(function (a, b) {
+        return b.total - a.total;
+    });
+
+    LIST_WORD_FREQUENCY_COUNTERS.innerHTML = "";
+    if (wordArray.length > 0) {
+        let liUW = document.createElement("li");
+        liUW.innerText = "number of unique words: " + wordArray.length;
+        LIST_WORD_FREQUENCY_COUNTERS.append(liUW);
+        let liTW = document.createElement("li");
+        liTW.innerText = "total number of words: " + TOKENIZED_DATA.length;
+        LIST_WORD_FREQUENCY_COUNTERS.append(liTW);
     }
-    for (let i = 0; i < keys.length; i++) {
+
+    LIST_WORD_FREQUENCY_LIST.innerHTML = "";
+    for (let i = 0; i < wordArray.length; i++) {
         let li = document.createElement("li");
-        li.innerText = keys[i] + ": " + values[i];
-        LIST_WORD_FREQUENCY.append(li);
+        li.innerText = wordArray[i].name + ": " + wordArray[i].total;
+        LIST_WORD_FREQUENCY_LIST.append(li);
     }
 }
