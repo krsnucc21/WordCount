86b7966328741d424c4279e9b3e229d2d424e89e
krsnucc21 <73293364+krsnucc21@users.noreply.github.com>
2020-10-24 19:44:19 +0900

    Update wordfreq.js

--- wordfreq.js
+++ wordfreq.js
@@ -1,4 +1,3 @@
-const MAX_FONT_SIZE = 64;
 const ID_WORD_FREQUENCY_LIST = "word_freq_list";
 const ID_INPUT_TEXT = "input_text";
 const LIST_WORD_FREQUENCY = document.getElementById(ID_WORD_FREQUENCY_LIST);
@@ -29,6 +28,7 @@ function showWordFreq() {
     const OBJECT_WORD_FREQUENCY = wordFreq(DATA);
     let keys = [];
     let values = [];
+    OBJECT_WORD_FREQUENCY.Sort();
     for (let [key, value] of Object.entries(OBJECT_WORD_FREQUENCY)) {
         keys.push(key);
         values.push(value);
@@ -36,13 +36,6 @@ function showWordFreq() {
     for (let i = 0; i < keys.length; i++) {
         let li = document.createElement("li");
         li.innerText = keys[i] + ": " + values[i];
-        let fontSize = 12;
-        if (fontSize + values[i] < MAX_FONT_SIZE) {
-            fontSize += values[i];
-        } else {
-            fontSize = MAX_FONT_SIZE;
-        }
-        li.style.fontSize = `${fontSize}px`;
         LIST_WORD_FREQUENCY.append(li);
     }
 }
